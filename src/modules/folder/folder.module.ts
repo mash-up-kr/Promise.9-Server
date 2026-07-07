4ada7758728271fc8694859e1131ae0cc8ef3b94
hyoinkang <hyoin1704@gmail.com>
2026-07-08 00:05:36 +0900

    feat: 링크 및 폴더 API에 JWT 인증 적용 및 유저 아이디 하드코딩 제거

--- src/modules/folder/folder.module.ts
+++ src/modules/folder/folder.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 
 import { DatabaseModule } from '../../config/database/database.module'
+import { AuthModule } from '../auth/auth.module'
 
 import { FolderController } from './folder.controller'
 import { FolderService } from './folder.service'
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, AuthModule],
     controllers: [FolderController],
     providers: [FolderService],
 })
--- src/modules/link/link.module.ts
+++ src/modules/link/link.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 
 import { DatabaseModule } from '../../config/database/database.module'
+import { AuthModule } from '../auth/auth.module'
 
 import { LinkController } from './link.controller'
 import { LinkService } from './link.service'
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, AuthModule],
     controllers: [LinkController],
     providers: [LinkService],
 })
