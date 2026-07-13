04222c329b096e109bbdb3038318ce966519f154
hyoinkang <hyoin1704@gmail.com>
2026-07-13 20:13:27 +0900

    feat: auth/user 컨트롤러 스웨거 응답 명세 추가

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,25 +6,27 @@ import {
     HttpStatus,
     Post,
 } from '@nestjs/common'
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger'
+import { ApiTags } from '@nestjs/swagger'
 
 import { ZodValidationPipe } from '../../common/pipe/zod-validation.pipe'
 
 import {
-    LogoutDto,
     LogoutInput,
     logoutSchema,
-    RefreshDto,
     RefreshInput,
     refreshSchema,
-    SocialLoginDto,
     SocialLoginInput,
     socialLoginSchema,
-    WithdrawDto,
     WithdrawInput,
     withdrawSchema,
 } from './dto/auth.dto'
 import { AuthService } from './auth.service'
+import {
+    ApiLogout,
+    ApiRefreshToken,
+    ApiSocialLogin,
+    ApiWithdraw,
+} from './auth.swagger'
 
 @ApiTags('auth')
 @Controller('auth')
@@ -33,8 +35,7 @@ export class AuthController {
 
     @Post('social')
     @HttpCode(HttpStatus.OK)
-    @ApiOperation({ summary: '소셜 로그인' })
-    @ApiBody({ type: SocialLoginDto })
+    @ApiSocialLogin()
     async socialLogin(
         @Body(new ZodValidationPipe(socialLoginSchema)) dto: SocialLoginInput,
     ) {
@@ -43,8 +44,7 @@ export class AuthController {
 
     @Post('refresh')
     @HttpCode(HttpStatus.OK)
-    @ApiOperation({ summary: '토큰 재발급' })
-    @ApiBody({ type: RefreshDto })
+    @ApiRefreshToken()
     async refresh(
         @Body(new ZodValidationPipe(refreshSchema)) dto: RefreshInput,
     ) {
@@ -53,8 +53,7 @@ export class AuthController {
 
     @Post('logout')
     @HttpCode(HttpStatus.NO_CONTENT)
-    @ApiOperation({ summary: '로그아웃' })
-    @ApiBody({ type: LogoutDto })
+    @ApiLogout()
     async logout(
         @Body(new ZodValidationPipe(logoutSchema)) dto: LogoutInput,
     ): Promise<void> {
@@ -63,8 +62,7 @@ export class AuthController {
 
     @Delete('withdraw')
     @HttpCode(HttpStatus.NO_CONTENT)
-    @ApiOperation({ summary: '회원 탈퇴' })
-    @ApiBody({ type: WithdrawDto })
+    @ApiWithdraw()
     async withdraw(
         @Body(new ZodValidationPipe(withdrawSchema)) dto: WithdrawInput,
     ): Promise<void> {
